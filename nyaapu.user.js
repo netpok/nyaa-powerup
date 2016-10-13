8e4653076e8537aa5862fdcfd6037086759a1e4f
netpok <netpok.reg@gmail.com>
2016-10-13 11:34:43 +0200

    Add qBittorrent links

--- nyaapu.user.js
+++ nyaapu.user.js
@@ -7,7 +7,7 @@
 // @match        http://www.nyaa.se/*
 // @match        https://www.nyaa.se/*
 // @require      http://code.jquery.com/jquery-3.1.1.min.js
-// @grant        none
+// @grant        GM_addStyle
 // ==/UserScript==
 /* jshint -W097 */
 'use strict';
@@ -17,4 +17,8 @@ $("#tabnav").find("li:nth(6)").find("a").click(function (e) {
     prompt("RSS URL:", this.href);
 });
 
-$("#main").find("div[class!='content']").remove()
+$("#main").find("div[class!='content']").remove();
+
+GM_addStyle(".qbHead{width: 20px;} .qbDownload{filter: hue-rotate(75deg);}");
+$(".tlistththree").before('<th class="qbHead">qB</th>');
+$(".tlistdownload").before('<td class="qbDownload"><a href="#"><img src="//files.nyaa.se/www-7.png" alt="Add to qBittorrent"></a></td>');
