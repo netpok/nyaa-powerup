ffeecd10e0d01d7dd1889ca9aff33e614f283437
netpok <netpok.reg@gmail.com>
2016-10-13 13:25:01 +0200

    Implement add torrent feature

--- nyaapu.user.js
+++ nyaapu.user.js
@@ -6,8 +6,9 @@
 // @author       netpok
 // @match        http://www.nyaa.se/*
 // @match        https://www.nyaa.se/*
-// @require      http://code.jquery.com/jquery-3.1.1.min.js
+// @require      https://code.jquery.com/jquery-3.1.1.min.js
 // @grant        GM_addStyle
+// @grant        GM_xmlhttpRequest
 // ==/UserScript==
 /* jshint -W097 */
 'use strict';
@@ -22,3 +23,25 @@ $("#main").find("div[class!='content']").remove();
 GM_addStyle(".qbHead{width: 20px;} .qbDownload{filter: hue-rotate(75deg);}");
 $(".tlistththree").before('<th class="qbHead">qB</th>');
 $(".tlistdownload").before('<td class="qbDownload"><a href="#"><img src="//files.nyaa.se/www-7.png" alt="Add to qBittorrent"></a></td>');
+$(".qbDownload").click(function (e) {
+    var url=$(this).next().find("a").prop('href');
+    GM_xmlhttpRequest({
+        method: "GET",
+        url: "https://localhost:8080/command/getSavePath",
+        onload: function (response) {
+            data = new FormData();
+            data.append('urls', url);
+            data.append('savepath', response.responseText + "/tv.shows.2ln");
+            data.append('label', "Anime");
+            data.append('cookie', "");
+            GM_xmlhttpRequest({
+                method: "POST",
+                url: "https://localhost:8080/command/download",
+                data: data,
+                onload: function (response, a) {
+                    console.log(response, a);
+                }
+            });
+        }
+    });
+});
